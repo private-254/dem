0394b075506f53d96c395258445681e3b620679a
private-254 <darrellcowgill081@gmail.com>
2025-10-09 15:12:11 +0300

    Update url.js

--- daveplugins/url.js
+++ daveplugins/url.js
@@ -89,7 +89,7 @@ async function urlCommand(sock, chatId, message) {
             return;
         }
 
-        await sock.sendMessage(chatId, { text: `Your Url: ${url}\n\n> 🏂June md urls` }, { quoted: message });
+        await sock.sendMessage(chatId, { text: `Your Url: ${url}\n\n> 🏂𝙳𝙰𝚅𝙴-𝙼𝙳 urls` }, { quoted: message });
     } catch (error) {
         console.error('[URL] error:', error?.message || error);
         await sock.sendMessage(chatId, { text: 'Failed to convert media to URL.' }, { quoted: message });
