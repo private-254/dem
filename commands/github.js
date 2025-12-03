70f005d0f9f7bee620c3549da339bfe533d224d0
𝐮𝐧𝐬𝐞𝐞𝐧 <darrellcowgill081@gmail.com>
2025-12-03 15:43:47 +0300

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -14,11 +14,11 @@ async function githubCommand(sock, chatId, message) {
             participants: "0@s.whatsapp.net",
             remoteJid: "status@broadcast",
             fromMe: false,
-            id: "ᴅᴀᴠᴇ-ᴍᴅ"
+            id: "Dave-verified"
         },
         message: {
             contactMessage: {
-                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ᴅᴀᴠᴇ-ᴍᴅ\nitem1.TEL;waid=${message.key.participant?.split('@')[0] || message.key.remoteJid.split('@')[0]}:${message.key.participant?.split('@')[0] || message.key.remoteJid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:DAVE-MD\nitem1.TEL;waid=${message.key.participant?.split('@')[0] || message.key.remoteJid.split('@')[0]}:${message.key.participant?.split('@')[0] || message.key.remoteJid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
             }
         },
         participant: "0@s.whatsapp.net"
@@ -30,50 +30,46 @@ async function githubCommand(sock, chatId, message) {
   const fkontak = createFakeContact(message);
     
 const pushname = message.pushName || "Unknown User";
-    const res = await fetch('https://api.github.com/repos/gifteddevsmd/DAVE-MD');
+    const res = await fetch('https://api.github.com/repos/gifteddevsmd/DAVE-XD');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
-    let txt = 
-           `🔹  \`𝙹𝚄𝙽𝙴  𝚁𝙴𝙿𝙾 𝙸𝙽𝙵𝙾.\` \n\n`;
-    txt += `🔸  *Name* : ${json.name}\n`;
-    txt += `🔸  *Watchers* : ${json.watchers_count}\n`;
-    txt += `🔸  *Size* : ${(json.size / 1024).toFixed(2)} MB\n`;
-    txt += `🔸  *Last Updated* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`;
-    txt += `🔸  *REPO* : ${json.html_url}\n\n`;    
-    txt += `🔹  *Forks* : ${json.forks_count}\n`;
-    txt += `🔹  *Stars* : ${json.stargazers_count}\n`;
-    txt += `🔹  *Desc* : ${json.description || 'None'}\n\n`;
-    txt += `Hey👋 ${pushname} _Thank you for choosing ᴅᴀᴠᴇ-ᴍᴅ, Fork-Star the repository_`;
+    let txt = `DaveMd Repo\n\n`;
+    txt += `Name: ${json.name}\n`;
+    txt += `Watchers: ${json.watchers_count}\n`;
+    txt += `Size: ${(json.size / 1024).toFixed(2)} MB\n`;
+    txt += `Last Updated: ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`;
+    txt += `Repo: ${json.html_url}\n\n`;    
+    txt += `Forks: ${json.forks_count}\n`;
+    txt += `Stars: ${json.stargazers_count}\n`;
+    txt += `Description: ${json.description || 'None'}\n\n`;
+    txt += `@${pushname} Thank you for choosing DAVE-MD, Star and fork the repository`;
 
     // Use the local asset image
     const imgPath = path.join(__dirname, '../assets/menu2.jpg');
     const imgBuffer = fs.readFileSync(imgPath);
 
-    /*await sock.sendMessage(chatId, { image: imgBuffer, caption: txt }, { quoted: message });*/
-               await sock.sendMessage(chatId, {
+    await sock.sendMessage(chatId, {
                 image: imgBuffer,
                 caption: txt,
                 contextInfo: {
                     forwardingScore: 1,
-                    isForwarded: false,
+                    isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '@newsletter',
-                        newsletterName: 'ᴅᴀᴠᴇ-ᴍᴅ Official',
+                        newsletterName: 'Dave Tech',
                         serverMessageId: -1
                     }
                 }
             },{ quoted: fkontak });   
       
-      
-      
-//arect sucess💉
+//arect sucess
     await sock.sendMessage(chatId, {
-            react: { text: '✔️', key: message.key }
+            react: { text: '🔥', key: message.key }
         });
     
   } catch (error) {
-    await sock.sendMessage(chatId, { text: '❌ Error fetching repository information.' }, { quoted: message });
+    await sock.sendMessage(chatId, { text: 'Error fetching repository information.' }, { quoted: message });
   }
 }
 
