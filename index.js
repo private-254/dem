5c936d3328170924fd6b4a6757c2595e256ad74d
𝐮𝐧𝐬𝐞𝐞𝐧 <darrellcowgill081@gmail.com>
2025-12-11 23:21:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,9 +32,7 @@ const {
     delay
 } = Baileys;
 
-const baileysPkg = await import('@whiskeysockets/baileys/package.json', { 
-  with: { type: "json" } 
-});
+import baileysPkg from '@whiskeysockets/baileys/package.json' with { type: "json" };
 import NodeCache from "node-cache";
 import pino from "pino";
 import readline from "readline";
